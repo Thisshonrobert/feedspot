807a9c1aef8821fff6aadf7ec850f26c8b307bab
Thisshonrobert <thisshonrobert0205@gmail.com>
2024-08-01 21:14:38 +0530

    signup added

--- frontend/FeedSpot/src/App.jsx
+++ frontend/FeedSpot/src/App.jsx
@@ -1,15 +1,19 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import '@radix-ui/themes/styles.css';
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      {/* <Route path='/signup' element={<Signup/>}></Route> */}
+     
+       <Route path='/signup' element={<Signup/>}></Route>
       <Route path='/signin' element={<Signin/>}></Route>
       {/* <Route path='/' element={<Dashboard/>}></Route> */}
+      
     </Routes>
   </BrowserRouter>
   )
--- frontend/FeedSpot/src/pages/Signup.jsx
+++ frontend/FeedSpot/src/pages/Signup.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaLock, FaMailBulk } from "react-icons/fa";
-import bg from '../assets/bg2.jpg'
+import bg from '../assets/bg.jpg'
 import { Button } from "@radix-ui/themes";
- 
-const Signin = () => {
+import { RiAccountBoxLine } from "react-icons/ri";
+const Signup = () => {
+    const[name,setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -15,8 +16,20 @@ const Signin = () => {
   return (
     <div className="relative h-screen flex items-center justify-center bg-cover bg-center brightness-175" style={{ backgroundImage: `url(${bg})` }}>
       <form className="relative z-10 bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-70 rounded-xl p-6 w-full max-w-md mx-4 sm:mx-0" onSubmit={handleLogin}>
-        <h1 className="text-center text-2xl text-white mb-8">Login to FEEDsPOT</h1>
+        <h1 className="text-center text-2xl text-white mb-8">Sigup to FEEDsPOT</h1>
         <div className="space-y-5 mb-6">
+        <div className="flex items-center">
+          <RiAccountBoxLine className="text-white text-xl mr-3" />
+          <input
+              type="text"
+              placeholder="User Name"
+             className="w-full bg-transparent backdrop-blur-lg border outline-none border-white text-white border-opacity-70 rounded-full px-5 py-3"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              defaultValue={""}
+              required
+            />
+          </div>
           <div className="flex items-center">
           <FaMailBulk className="text-white text-xl mr-3" />
           <input
@@ -40,10 +53,10 @@ const Signin = () => {
 
         </div>
         <Button align='center' size='4' color="cyan" variant="soft"  gap='3' type="submit" className="ml-[40%]">
-          Login
+          Signup
         </Button>
         <div className="text-center text-sm text-white mt-4">
-          Don't have an account? <a href="#" className="font-semibold hover:underline text-red-600">Register</a>
+          Already have an account? <a href="#" className="font-semibold hover:underline text-red-600">Login</a>
         </div>
       </form>
       <div className="absolute inset-0 bg-black bg-opacity-60 z-0"></div>
@@ -51,4 +64,4 @@ const Signin = () => {
   );
 };
 
-export default Signin;
+export default Signup;
